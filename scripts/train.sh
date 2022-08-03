461fce4f8d986f6f21c417738e30e274f6180af2
zhanghm1995 <zhanghm_1995@qq.com>
2022-08-03 16:54:51 +0800

    update scripts to support training in AIStation

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,8 +1,22 @@
-set -x
-
 ## The offical script
 # python hififace_trainer.py --model_config config/model.yaml --train_config config/trainer.yaml -n hififace
 
-python hififace_trainer.py --model_config config/model_HDTF.yaml \
-                           --train_config config/trainer_HDTF.yaml \
-                           -n hififace_expression
+ZHM_PATH="/221019051/"
+YZH_PATH="/221019041/"
+
+PYTHON=python
+if [ -d "$ZHM_PATH" -o -d "$YZH_PATH" ]; then
+    echo "In AIStation platform"
+    PYTHON=/root/miniconda3/envs/py36-torch100-cu11/bin/python
+    if [ -f "$PYTHON" ]; then
+        PYTHON=/root/miniconda3/envs/py36-torch100-cu11/bin/python
+    else
+        PYTHON=python
+    fi
+    cd /221019051/Research/Face/hififace
+fi
+
+set -x
+${PYTHON} hififace_trainer.py --model_config config/model_HDTF.yaml \
+                              --train_config config/trainer_HDTF.yaml \
+                              -n hififace_expression
